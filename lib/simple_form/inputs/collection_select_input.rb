52d24d8b4e28b32db281de982dc108b8d9609947
Johan Buts <johan.buts@trustedfamily.net>
2013-12-12 13:53:56 +0100

    Ability to specify collection_options directly to a select input instead of using the collection builder

--- lib/simple_form/inputs/collection_select_input.rb
+++ lib/simple_form/inputs/collection_select_input.rb
@@ -2,12 +2,19 @@ module SimpleForm
   module Inputs
     class CollectionSelectInput < CollectionInput
       def input
-        label_method, value_method = detect_collection_methods
+        if !options[:collection_options].blank?
+          restricted_input_options = input_html_options.dup
+          restricted_input_options.merge!(input_options.select{ |k,v| k.in?(:multiple, :disabled, :include_blank, :prompt) })
 
-        @builder.collection_select(
-          attribute_name, collection, value_method, label_method,
-          input_options, input_html_options
-        )
+          template.select_tag("#{indexed_object_name}[#{attribute_name}]", options[:collection_options], restricted_input_options)
+        else
+          label_method, value_method = detect_collection_methods
+
+          @builder.collection_select(
+            attribute_name, collection, value_method, label_method,
+            input_options, input_html_options
+          )
+        end
       end
     end
   end
